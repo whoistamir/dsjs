c5fd124ba2db72c3b12893feb9dc6fca4bc10620
Tamir <contact@tamir.it>
2022-07-09 13:27:54 -0700

    Added completed implementation of Queue

--- queue.js
+++ queue.js
@@ -2,4 +2,38 @@ class myQueue{
   constructor(){
     this.queue = [];
   }
+
+  print(){
+    console.log(this.queue);
+  }
+
+  enqueue(val){
+    this.queue.push(val);
+  }
+
+  dequeue(val){
+    this.queue.shift();
+  }
+
+  front(){
+    return this.queue[0];
+  }
+
+  size(){
+    return this.queue.length;
+  }
+
+  isEmpty(){
+    return this.size === 0;
+  }
 }
+
+//Tests
+let q = new myQueue();
+q.enqueue('a');
+q.enqueue('b');
+q.enqueue('c');
+q.print();
+q.dequeue();
+console.log(q.front());
+q.print();
